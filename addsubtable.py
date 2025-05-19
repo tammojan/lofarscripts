26059232a32c82361db622dd17c16e9cd3f04b86
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2025-05-19 16:06:50 +0200

    Update addsubtable

--- addsubtable.py
+++ addsubtable.py
@@ -1,39 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from pyrap.tables import table
+import casacore.tables as pt
 from os import path
 import sys
 from shutil import copytree
 import argparse
 
-def addsubtable(msfile, subtablefile, subtablename=None):
-  ''' Adds an existing table as a subtable to another table'''
-  ''' The subtable is copied into the MS if necessary '''
 
-  if subtablename is None:
-    subtablename=path.basename(subtablefile.rstrip('/'))
-  else:
-    if subtablefile.startswith(msfile):
-      print "Error: when the subtable is already in the MS, don't specify a new name for it"
-      sys.exit(1)
+def addsubtable(msfile, subtablefile, dry_run=False):
+    """Adds an existing table as a subtable to another table
+    The subtable is copied into the MS if it is not a subdirectory already"""
 
-  if not subtablefile.startswith(msfile):
-    print "Copying", subtablefile, " into", path.join(msfile, subtablename) 
-    copytree(subtablefile, path.join(msfile, subtablename))
-    subtablefile=path.join(msfile, subtablename)
+    subtablename = path.basename(subtablefile.rstrip("/"))
 
-  t=table(msfile, readonly=False, ack=False)
-  sub=table(subtablefile, ack=False)
-  t.putkeyword(subtablename, sub, makesubrecord=True)
-  sub.close()
-  t.close()
+    if path.commonpath([msfile, subtablefile]) != path.commonpath([msfile]):
+        print(f"Copying {subtablefile} into {path.join(msfile, subtablename)}")
+        if not dry_run:
+            copytree(subtablefile, path.join(msfile, subtablename))
+        subtablefile = path.join(msfile, subtablename)
 
-if __name__ == '__main__':
-  parser=argparse.ArgumentParser(description="Adds an existing table (e.g. instrument) to another table")
-  parser.add_argument("ms", help="Master measurement set")
-  parser.add_argument("subtable", help="Subtable to add")
-  parser.add_argument("-s", "--subtablename", help="Name of subtable (defaults to its filename)", default=None)
-  
-  args=parser.parse_args()
+    t = pt.table(msfile, readonly=False, ack=False)
+    sub = pt.table(subtablefile, ack=False)
+    if not dry_run:
+        t.putkeyword(subtablename, sub, makesubrecord=True)
+    else:
+        print(f"Would make {subtablename} a subtable of {msfile}")
+    sub.close()
+    t.close()
 
-  addsubtable(args.ms, args.subtable, args.subtablename) 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Copy an existing table (e.g. PHASED_ARRAY) to another table as a subtable"
+    )
+    parser.add_argument("ms", help="Master measurement set")
+    parser.add_argument("subtable", help="Subtable to add")
+    parser.add_argument("-n", "--dry-run", help="Dry run", action='store_true')
+
+    args = parser.parse_args()
+
+    addsubtable(args.ms, args.subtable, args.dry_run)
